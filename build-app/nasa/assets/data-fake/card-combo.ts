a92aa3ee21f89c424b4974fcb30555ac8d634273
trungkienpham-dev <139516851+trungkienpham-dev@users.noreply.github.com>
2024-07-05 23:05:17 +0700

    build app

---
+++ build-app/nasa/assets/data-fake/card-combo.ts
@@ -0,0 +1,241 @@
+import { product, introProd } from "src/app/core/models/product.model"
+
+export const satMyThuat: product[] = [
+  {
+    id: '0',
+    title: 'Cổng',
+    desc: 'mô tả ngắn',
+    image: 'https://i.pinimg.com/564x/e7/95/de/e795de55428920eefb5ee3ae498edaef.jpg',
+    imageLst: [
+      'https://i.pinimg.com/474x/7b/f3/4b/7bf34b42dfba782b02239ffcd2614d48.jpg',
+      'https://i.pinimg.com/474x/27/04/2f/27042fb6a1c7da3a2c65643edd595ee7.jpg',
+      'https://i.pinimg.com/474x/15/70/90/157090604c130ceb65a526bada79af5a.jpg',
+      'https://i.pinimg.com/474x/bf/2b/6c/bf2b6c7eba60a0efcdcc48f5372d933c.jpg',
+      'https://i.pinimg.com/474x/6b/bb/e3/6bbbe357c37b14cfddf827899faa8012.jpg',
+      'https://i.pinimg.com/474x/42/e8/4d/42e84d7985d52af5a5b839b7d8e95023.jpg'
+    ],
+    path: 'sat-my-thuat/gate',
+    content: ''
+  },
+  {
+    id: '1',
+    title: 'Mái',
+    desc: 'mô tả ngắn',
+    image: 'https://i.pinimg.com/474x/fc/c8/8f/fcc88f44b444b9a2002dced22ef7eb7e.jpg',
+    imageLst: [],
+    path: 'sat-my-thuat/roof',
+    content: ""
+  },
+  {
+    id: '2',
+    title: 'Lan can',
+    desc: 'Mô tả ngắn',
+    image: 'https://i.pinimg.com/474x/ef/4b/ae/ef4bae27c15d837d028f1ea259197f87.jpg',
+    imageLst: [],
+    path: 'sat-my-thuat/handrail',
+    content: ''
+  },
+  {
+    id: '3',
+    title: 'Cầu thang',
+    desc: 'Mô tả ngắn',
+    image: 'https://i.pinimg.com/474x/c1/14/01/c11401ae86a0312f43ee5c05a1644565.jpg',
+    imageLst: [],
+    path: 'sat-my-thuat/stair',
+    content: ''
+  },
+]
+
+
+export const nhomkinh: product[] = [
+  {
+    id: '0',
+    title: 'Thuỷ lực',
+    desc: 'Mô tả ngắn sản phẩm nhôm kính',
+    image: 'https://i.pinimg.com/474x/87/c1/c4/87c1c427e61e2f72f441c48968f5f13a.jpg',
+    imageLst: [],
+    path: 'nhom-kinh/hydraulic',
+    content: ''
+  },
+  {
+    id: '1',
+    title: 'Xếp trượt',
+    desc: 'Mô tả ngắn sản phẩm nhôm kính',
+    image: 'https://i.pinimg.com/474x/37/97/b5/3797b5e72d5b6967a50a70752037548c.jpg',
+    imageLst: [],
+    path: 'nhom-kinh/sliding',
+    content: ''
+  },
+  {
+    id: '2',
+    title: 'Trượt quay',
+    desc: 'Mô tả ngắn sản phẩm nhôm kính',
+    image: 'https://i.pinimg.com/474x/81/85/ca/8185ca73b271363013a70119abb56127.jpg',
+    imageLst: [],
+    path: 'nhom-kinh/rotating',
+    content: ''
+  },
+  {
+    id: '3',
+    title: 'Mở quay, mở lùa',
+    desc: 'Mô tả ngắn sản phẩm nhôm kính',
+    image: 'https://i.pinimg.com/474x/d4/19/e7/d419e784d313197d9d4f22c6379d94b5.jpg',
+    imageLst: [],
+    path: 'nhom-kinh/swing',
+    content: ''
+  },
+]
+
+export const outstandingProducts: product[] = [
+  {
+    id: '0',
+    title: 'cổng sắt mỹ thuật 1',
+    desc: 'mô tảcổng sắt mỹ thuật 1',
+    image: 'https://i.pinimg.com/564x/e7/95/de/e795de55428920eefb5ee3ae498edaef.jpg',
+    imageLst: [],
+    path: '',
+    content: ''
+  },
+  {
+    id: '1',
+    title: 'cổng sắt mỹ thuật 2',
+    desc: 'cổng sắt mỹ thuật 2',
+    image: 'https://i.pinimg.com/564x/07/bd/4b/07bd4bad61d666f3c9b9e6be46e2ee4a.jpg',
+    imageLst: [],
+    path: '',
+    content: ''
+  },
+  {
+    id: '2',
+    title: 'Mái sắt mỹ thuật',
+    desc: 'Mô tả ngắn mái sắt mỹ thuật',
+    image: 'https://i.pinimg.com/474x/8b/ae/23/8bae238b2cca51a416c64ee184a9f2c4.jpg',
+    imageLst: [],
+    path: '',
+    content: ''
+  },
+  {
+    id: '3',
+    title: 'Mái sắt mỹ thuật 2',
+    desc: 'Mô tả ngắn Mái sắt mỹ thuật 2',
+    image: 'https://i.pinimg.com/474x/fc/c8/8f/fcc88f44b444b9a2002dced22ef7eb7e.jpg',
+    imageLst: [],
+    path: '',
+    content: ''
+  },
+  {
+    id: '4',
+    title: 'lan can sắt mỹ thuật',
+    desc: 'Mô tả ngắn lan can sắt mỹ thuật',
+    image: 'https://i.pinimg.com/474x/99/7d/56/997d5617761ee90484086ffe05ee335d.jpg',
+    imageLst: [],
+    path: '',
+    content: ''
+  },
+  {
+    id: '5',
+    title: 'cầu thang sắt mỹ thuật',
+    desc: 'Mô tả ngắn cầu thang sắt mỹ thuật',
+    image: 'https://i.pinimg.com/474x/d3/6f/c3/d36fc35211edb9b510cbc6730026d68d.jpg',
+    imageLst: [],
+    path: '',
+    content: ''
+  },
+]
+
+export const gate: product = {
+  id: '',
+  title: 'cổng sắt mỹ thuật',
+  desc: 'Cổng sắt mỹ thuật với thiết kế độc đáo, sang trọng và lộng lẫy hiện đang rất được ưa thích. Cổng sắt nghệ thuật thích hợp với những gia chủ yêu thích phong cách cổ điển, tân cổ điển và cả hiện đại. Với tính thẩm mỹ, độ an toàn và độ bền cao, cổng sắt nghệ thuật chắc chắn sẽ là lựa chọn tuyệt vời cho mái ấm của bạn. Dưới đây thông tin và TOP những mẫu cửa cổng sắt mỹ nghệ đẹp và cao cấp của chúng tôi cung cấp.',
+  image: 'https://i.pinimg.com/564x/e7/95/de/e795de55428920eefb5ee3ae498edaef.jpg',
+  imageLst: [],
+  path: '',
+  content: ''
+}
+
+// [
+//   {
+//     name: 'SAT_MY_THUAT',
+//     tit: 'Sắt mỹ thuật',
+//     child: [
+//       {
+//         path: 'Gate',
+//         name: 'Cổng'
+//       },
+//       {
+//         path: 'Roof',
+//         name: 'Mái'
+//       },
+//       {
+//         path: 'Handrail',
+//         name: 'Lan can'
+//       },
+//       {
+//         path: 'Stair',
+//         name: 'Cầu thang'
+//       }
+//     ]
+//   },
+//   {
+//     name: 'NHOM_KINH',
+//     tit: 'Nhôm kính',
+//     child: [
+//       {
+//         path: 'Hydraulic',
+//         name: 'Thuỷ lực'
+//       },
+//       {
+//         path: 'Sliding',
+//         name: 'Xếp trượt'
+//       },
+//       {
+//         path: 'Rotating',
+//         name: 'Trượt quay'
+//       },
+//       {
+//         path: 'Swing',
+//         name: 'Mở quay, mở lùa'
+//       },
+//       {
+//         path: 'Xingfa',
+//         name: 'Cửa nhôm Xingfa'
+//       },
+//       {
+//         path: 'Chamfered55',
+//         name: 'Cửa nhôm vát cạnh 55'
+//       }
+//     ]
+//   }
+// ]
+// [
+//   {
+//     name: 'Hydraulic',
+//     title: 'Thuỷ lực',
+//     img: 'https://i.pinimg.com/474x/44/5b/bd/445bbda641f2b22e71970d15be568f24.jpg'
+//   },
+//   {
+//     name: 'Sliding',
+//     title: 'Xếp trượt',
+//     img: 'https://i.pinimg.com/474x/44/5b/bd/445bbda641f2b22e71970d15be568f24.jpg'
+//   },
+//   {
+//     name: 'Rotating',
+//     title: 'Trượt quay',
+//     img: 'https://i.pinimg.com/474x/44/5b/bd/445bbda641f2b22e71970d15be568f24.jpg'
+//   },
+//   {
+//     name: 'Swing',
+//     title: 'Mở quay mở lùa',
+//     img: 'https://i.pinimg.com/474x/44/5b/bd/445bbda641f2b22e71970d15be568f24.jpg'
+//   },
+//   {
+//     name: 'Xingfa',
+//     title: 'Cửa nhôm Xingfa',
+//     img: 'https://i.pinimg.com/474x/44/5b/bd/445bbda641f2b22e71970d15be568f24.jpg'
+//   },
+//   {
+//     name: 'Chamfered55',
+//     title: 'Cửa nhôm vát cạnh 55',
+//     img: 'https://i.pinimg.com/474x/44/5b/bd/445bbda641f2b22e71970d15be568f24.jpg'
+//   }
+// ]
